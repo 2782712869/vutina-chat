4eb1bda7ae54decab0bc56211ea9bcd3ea18c6ce
2782712869 <2782712869@qq.com>
2024-01-12 11:41:08 +0800

    fix: remove files

--- server/src/helpers/index.ts
+++
@@ -1,7 +0,0 @@
-import crypto from 'crypto'
-
-export const hashPassword = (password: string) => {
-  const hash = crypto.createHash('sha256');
-  hash.update(password);
-  return hash.digest('hex');
-};
